6cc71971693ce7b446179c74e2fe871e8adee06b
reshmamullappally <reshmamullappally@gmail.com>
2025-08-14 14:36:06 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Student from "./Student"
+
+function App() {
+    return(
+      <>
+      <Student name="Reshma" age={34} isStudent={true}/>
+       <Student name="Priya" age={25} isStudent={true}/>
+       <Student name="Leela" age={88} isStudent={false}/>
+       <Student name="fdfdsf" />
+       </>
+    )
+}
+
+export default App
